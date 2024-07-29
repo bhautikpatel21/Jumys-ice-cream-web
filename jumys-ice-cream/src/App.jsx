c3be8698b9e5c916e8bbb9116cff1a2c14b27acb
bhautik dudhat <bdudhat293@gmail.com>
2024-07-29 19:38:07 +0530

    react

--- jumys-ice-cream/src/App.jsx
+++ jumys-ice-cream/src/App.jsx
@@ -1,18 +1,41 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
-import img1 from './assets/asset 0.svg'
-import img2 from './assets/asset 62.svg'
+import DropdownMenu from './Components/DropdownMenu'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  // const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  // const handleMouseEnter = () => {
+  //   setDropdownVisible(true);
+  // };
+
+  // const handleMouseLeave = () => {
+  //   setDropdownVisible(false);
+  // };
 
   return (
     <>
-
-      <Navbar/>
-     
+    <Navbar/>
     </>
+    // <>
+    //   <Navbar/>
+    //   <div className="App">
+    //   <header className="App-header">
+    //     <div
+    //       className="menu"
+    //       onMouseEnter={handleMouseEnter}
+    //       onMouseLeave={handleMouseLeave}
+    //     >
+    //       <button>Dropdown Menu</button>
+    //       {/* <DropdownMenu /> */}
+    //       {isDropdownVisible && <DropdownMenu />}
+    //     </div>
+    //   </header>
+    // </div>
+    // </>
   )
 }
 
--- jumys-ice-cream/src/Components/Navbar.jsx
+++ jumys-ice-cream/src/Components/Navbar.jsx
@@ -2,11 +2,41 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import './Navbar.css'
 import { FaAngleDown, FaBars,FaGripVertical, FaHeart, FaSearch, FaShoppingBasket, FaUser } from "react-icons/fa";
+import { useState } from 'react';
+
 
 const Navbar = () => {
+
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const HomeMenu = () => {
+    return(
+      <div className="dropdown-menu">
+        <ul>
+          <li>Home 1</li>
+          <li>Home 2</li>
+          <li>Home 3</li>
+          <li>Home 4</li>
+          <li>Home 5</li>
+          <li>Home 6</li>
+          <li>Home 7</li>
+          <li>Home 8</li>
+        </ul>
+      </div>
+    )
+  }
+  
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+  
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
+
   return (
     <>
-    <div className='w-full h-28 flex gap-2 justify-around bg-white fixed' id='layout'>
+    <div className='w-full h-28 flex gap-2 justify-around bg-white ' id='layout'>
       <div className='w-11 h-full flex justify-center items-center text-[30px]' id='menuBar'>
         <FaBars/>
       </div>
@@ -16,7 +46,13 @@ const Navbar = () => {
       </div>
 
       <div className='w-[600px] h-full flex items-center justify-around font-semibold' id='pages'>
+
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <a className='flex' id='underline-animate' href="">Home<FaAngleDown className='mt-[5px] ml-1'/></a>
+        {/* {HomeMenu()} */}
+        {dropdownVisible && HomeMenu()}
+        </div>
+
         <a className='flex' id='underline-animate' href="">Shop<FaAngleDown className='mt-[5px] ml-1'/></a>
         <a className='flex' id='underline-animate' href="">Products<FaAngleDown className='mt-[5px] ml-1'/></a>
         <a className='flex' id='underline-animate' href="">Blog<FaAngleDown className='mt-[5px] ml-1'/></a>
@@ -30,7 +66,6 @@ const Navbar = () => {
         <a className='icon4' href=""><FaShoppingBasket/></a>
       </div>
     </div>
-    1022222225
     <div className='w-full h-[80px] fixed bottom-0 flex justify-around bg-white' id='header-footer'>
       <a className='w-24 h-full flex flex-col justify-center items-center'><FaGripVertical className='text-[30px]'/><p>Shop</p></a>
       <a className='w-24 h-full flex flex-col justify-center items-center'><FaUser className='text-[30px]'/><p>Account</p></a>

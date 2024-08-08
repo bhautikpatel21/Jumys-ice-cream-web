62811a56f36154149969a8a80fec63459f670b41
bhautik dudhat <bdudhat293@gmail.com>
2024-08-08 18:38:45 +0530

    react

--- jumys-ice-cream/src/App.jsx
+++ jumys-ice-cream/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Pages/Home'
 import Error404 from './Pages/Error404'
 import AboutUs from './Pages/AboutUsPage'
 import Contact from './Pages/Contact'
+import Faq2 from './Pages/Faq2'
 // import ProductCard from './Components/Card'
 
 function App() {
@@ -12,11 +13,12 @@ function App() {
 
   return (
     <>
-    <Home/> 
+    {/* <Home/>  */}
     {/* <BlogCard/> */}
     {/* <Error404/>  */}
     {/* <AboutUs/> */}
     {/* <Contact/> */}
+    <Faq2/>
     </>
 
   )

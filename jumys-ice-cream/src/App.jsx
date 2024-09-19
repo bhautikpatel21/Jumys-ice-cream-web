ab0fe641a8dc7c25cba2653e5b6ba7741d6bfebb
bhautik dudhat <bdudhat293@gmail.com>
2024-09-19 18:39:21 +0530

    react

--- jumys-ice-cream/src/App.jsx
+++ jumys-ice-cream/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Layout from './Pages/Layout'
+import ShopList from './Pages/ShopList'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Layout/>
+    {/* <Layout/> */}
+    <ShopList/>
     </>
 
   )

af56c010cf8d082cafb122c8a16d155f785582aa
Bhautik Dudhat <bdudhat293@gmail.com>
2024-10-02 12:47:29 +0530

    H make comp

--- jumys-ice-cream/src/App.jsx
+++ jumys-ice-cream/src/App.jsx
@@ -1,13 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Layout from './pages/Layout'
-import ProductCardProductList from './Components/ProductCardList' 
-import FilterComponent from './Components/FilterComponent'
-import ProductCardList from './Components/ProductCardList'
-import ShopList from './pages/ShopList'
-import List_Renderingg from './Components/Cards_slider'
-// import ShopProduct from './Components/ShopProduct'
-import ShopProduct from './Components/renderOnSlide'
+import ShopProduct from './Components/ShopProduct'
 
 function App() {
 
@@ -15,9 +9,8 @@ function App() {
     <>
 
     {/* <Layout/> */}
-    {/* <ShopList/> */}
     <ShopProduct/>
-
+    
     </>
 
   )

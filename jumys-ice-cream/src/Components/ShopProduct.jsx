1f6217ae55f127eeba4d5e17dfb9e0fd8dac496e
Mahendra <mahendrapurohit9889@gmail.com>
2024-10-05 09:07:58 +0530

    shop page

--- jumys-ice-cream/src/Components/ShopProduct.jsx
+++ jumys-ice-cream/src/Components/ShopProduct.jsx
@@ -366,8 +366,7 @@
 
 // export default ShopProduct;
 
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import React, { useState, useEffect } from 'react';
 import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
@@ -426,6 +425,7 @@ import img25B from "../assets/asset 114.jpeg";
 const ShopProduct = () => {
     const [data, setData] = useState([]);
     const [visibleIndices, setVisibleIndices] = useState({});
+    const [hoveredIndex, setHoveredIndex] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
     const [columns, setColumns] = useState(2);
     const productsPerPage = 12;
@@ -433,183 +433,180 @@ const ShopProduct = () => {
     const Product = [
         {
             name: "Banana Cream Pudding",
-            rev: "1 Review", 
-            price: "$12.00", 
-            img1: img1A, 
+            rev: "1 Review",
+            price: "$12.00",
+            img1: img1A,
             img2: img1B
-          },
-          { 
-            name: "Blackout Chocolate Cake", 
-            rev: "2 Reviews", 
-            price: "$12.00", 
-            img1: img2A, 
-            img2: img2B 
-          },
-          { 
-            name: "Brambleberry Crisp", 
-            rev: "1 Review", 
-            price: "$10.00", 
-            img1: img3A, 
-            img2: img3B 
-          },
-          { 
-            name: "Brown Sugar Cinnamon", 
-            rev: "0 Reviews", 
-            price: "$15.00", 
-            img1: img4A, 
-            img2: img4B 
-          },
-          { 
-            name: "Burnt Orange Dreamsicle", 
-            rev: "0 Reviews", 
-            price: "$12.00", 
-            img1: img5A, 
-            img2: img5B 
-          },
-          { 
-            name: "Chocolate Mud", 
-            rev: "0 Reviews", 
-            price: "$15.00", 
-            img1: img6A, 
-            img2: img6B 
-          },
-          { 
-            name: "Cold Brew with Coconut Cream", 
-            rev: "0 Reviews", 
-            price: "$12.00", 
-            img1: img7A, 
-            img2: img7B 
-          },
-          {
-            name: "Cookies in Cream", 
-            rev: "0 Reviews", 
-            price: "$13.00", 
-            img1: img8A, 
-            img2: img8B 
-          },
-          { 
-            name: "Cream Puff", 
-            rev: "1 Review", 
-            price: "$11.00", 
-            img1: img9A, 
-            img2: img9B 
-          },
-          { 
-            name: "Darkest Chocolate", 
-            rev: "0 Reviews", 
-            price: "$13.00", 
-            img1: img10A, 
-            img2: img10B 
-          },
-          { 
-            name: "Double Dough", 
-            rev: "0 Reviews", 
-            price: "$10.00", 
-            img1: img11A, 
-            img2: img11B 
-          },
-          { 
-            name: "Frosé Sorbet", 
-            rev: "1 Review", 
-            price: "$12.00", 
-            img1: img12A, 
-            img2: img12B 
-          },
-      
-      
-          { 
-            name: "Golden Nectar", 
-            rev: "1 Review", 
-            price: "$10.00", 
-            img1: img13A, 
-            img2: img13B 
-          }, 
-          { 
-            name: "Green Mint Chip", 
-            rev: "1 Review", 
-            price: "$9.00", 
-            img1: img14A, 
-            img2: img14B 
-          },
-          { 
-            name: "Hot Toddy Sorbet", 
-            rev: "0 Review", 
-            price: "$12.00", 
-            img1: img15A, 
-            img2: img15B 
-          },
-          { 
-            name: "Pei Apple Crisp", 
-            rev: "1 Review", 
-            price: "$13.00", 
-            img1: img16A, 
-            img2: img16B 
-          },
-          { 
-            name: "Powdered Jelly Donut", 
-            rev: "0 Review", 
-            price: "$9.00", 
-            img1: img17A, 
-            img2: img17B 
-          },
-          { 
-            name: "Salty Caramel", 
-            rev: "0 Review", 
-            price: "$11.00", 
-            img1: img18A, 
-            img2: img18B 
-          },
-          { 
-            name: "Snow Cone Sorbet", 
-            rev: "0 Review", 
-            price: "$10.00", 
-            img1: img19A, 
-            img2: img19B 
-          },
-          { 
-            name: "Sorbet Street Treats", 
-            rev: "1 Review", 
-            price: "$10.00", 
-            img1: img20A, 
-            img2: img20B 
-          },
-          { 
-            name: "Sparkling Cherry Pie", 
-            rev: "0 Review", 
-            price: "$11.00", 
-            img1: img21A, 
-            img2: img21B 
-          },
-          { 
-            name: "Sunshine", 
-            rev: "1 Review", 
-            price: "$10.00", 
-            img1: img22A, 
-            img2: img22B 
-          },
-          { 
-            name: "Tahini Oat Chocolate Cookies", 
-            rev: "1 Review", 
-            price: "$10.00", 
-            img1: img23A, 
-            img2: img23B 
-          },
-          { 
-            name: "Golden Nectar", 
-            rev: "1 Review", 
-            price: "$10.00", 
-            img1: img24A, 
-            img2: img24B 
-          },
-          { 
-            name: "Zildberry Lavender", 
-            rev: "0 Review", 
-            price: "$13.00", 
-            img1: img25A, 
-            img2: img25B 
-          },
-      
-     ];
+        },
+        {
+            name: "Blackout Chocolate Cake",
+            rev: "2 Reviews",
+            price: "$12.00",
+            img1: img2A,
+            img2: img2B
+        },
+        {
+            name: "Brambleberry Crisp",
+            rev: "1 Review",
+            price: "$10.00",
+            img1: img3A,
+            img2: img3B
+        },
+        {
+            name: "Brown Sugar Cinnamon",
+            rev: "0 Reviews",
+            price: "$15.00",
+            img1: img4A,
+            img2: img4B
+        },
+        {
+            name: "Burnt Orange Dreamsicle",
+            rev: "0 Reviews",
+            price: "$12.00",
+            img1: img5A,
+            img2: img5B
+        },
+        {
+            name: "Chocolate Mud",
+            rev: "0 Reviews",
+            price: "$15.00",
+            img1: img6A,
+            img2: img6B
+        },
+        {
+            name: "Cold Brew with Coconut Cream",
+            rev: "0 Reviews",
+            price: "$12.00",
+            img1: img7A,
+            img2: img7B
+        },
+        {
+            name: "Cookies in Cream",
+            rev: "0 Reviews",
+            price: "$13.00",
+            img1: img8A,
+            img2: img8B
+        },
+        {
+            name: "Cream Puff",
+            rev: "1 Review",
+            price: "$11.00",
+            img1: img9A,
+            img2: img9B
+        },
+        {
+            name: "Darkest Chocolate",
+            rev: "0 Reviews",
+            price: "$13.00",
+            img1: img10A,
+            img2: img10B
+        },
+        {
+            name: "Double Dough",
+            rev: "0 Reviews",
+            price: "$10.00",
+            img1: img11A,
+            img2: img11B
+        },
+        {
+            name: "Frosé Sorbet",
+            rev: "1 Review",
+            price: "$12.00",
+            img1: img12A,
+            img2: img12B
+        },
+        {
+            name: "Golden Nectar",
+            rev: "1 Review",
+            price: "$10.00",
+            img1: img13A,
+            img2: img13B
+        },
+        {
+            name: "Green Mint Chip",
+            rev: "1 Review",
+            price: "$9.00",
+            img1: img14A,
+            img2: img14B
+        },
+        {
+            name: "Hot Toddy Sorbet",
+            rev: "0 Review",
+            price: "$12.00",
+            img1: img15A,
+            img2: img15B
+        },
+        {
+            name: "Pei Apple Crisp",
+            rev: "1 Review",
+            price: "$13.00",
+            img1: img16A,
+            img2: img16B
+        },
+        {
+            name: "Powdered Jelly Donut",
+            rev: "0 Review",
+            price: "$9.00",
+            img1: img17A,
+            img2: img17B
+        },
+        {
+            name: "Salty Caramel",
+            rev: "0 Review",
+            price: "$11.00",
+            img1: img18A,
+            img2: img18B
+        },
+        {
+            name: "Snow Cone Sorbet",
+            rev: "0 Review",
+            price: "$10.00",
+            img1: img19A,
+            img2: img19B
+        },
+        {
+            name: "Sorbet Street Treats",
+            rev: "1 Review",
+            price: "$10.00",
+            img1: img20A,
+            img2: img20B
+        },
+        {
+            name: "Sparkling Cherry Pie",
+            rev: "0 Review",
+            price: "$11.00",
+            img1: img21A,
+            img2: img21B
+        },
+        {
+            name: "Sunshine",
+            rev: "1 Review",
+            price: "$10.00",
+            img1: img22A,
+            img2: img22B
+        },
+        {
+            name: "Tahini Oat Chocolate Cookies",
+            rev: "1 Review",
+            price: "$10.00",
+            img1: img23A,
+            img2: img23B
+        },
+        {
+            name: "Golden Nectar",
+            rev: "1 Review",
+            price: "$10.00",
+            img1: img24A,
+            img2: img24B
+        },
+        {
+            name: "Zildberry Lavender",
+            rev: "0 Review",
+            price: "$13.00",
+            img1: img25A,
+            img2: img25B
+        },
+    ];
 
     useEffect(() => {
         setData(Product);
@@ -620,12 +617,12 @@ const ShopProduct = () => {
             if (window.innerWidth < 1000) {
                 setColumns(2);
             } else {
-                setColumns(4); 
+                setColumns(4);
             }
         };
 
         window.addEventListener('resize', updateColumns);
-        updateColumns(); 
+        updateColumns();
 
         return () => {
             window.removeEventListener('resize', updateColumns);
@@ -656,7 +653,7 @@ const ShopProduct = () => {
 
     return (
         <div>
-            <div className='mt-14 gap-8 w-full h-12 flex items-center mb-8 justify-end '>
+            <div className='mt-14 gap-8 w-full h-12 flex items-center mb-8 justify-end'>
                 <a href=""><FaList className="text-gray-600 text-2xl hover:text-red-600 " /></a>
                 {/* Only show menu icons if width is greater than 1000px */}
                 {window.innerWidth >= 1000 && (
@@ -698,17 +695,31 @@ const ShopProduct = () => {
             <div className={`grid grid-cols-${columns} gap-4`}>
                 {currentProducts.map((product, index) => (
                     <Card key={index} style={{ width: '100%', maxWidth: '250px', margin: 'auto' }}>
-                        <div className='relative' onMouseEnter={() => setVisibleIndices(prev => ({ ...prev, [index]: true }))} onMouseLeave={() => setVisibleIndices(prev => ({ ...prev, [index]: false }))}>
-                            <Card.Img variant="top" src={product.img1} alt={product.name} />
+                        <div className='relative'
+                            onMouseEnter={() => {
+                                setHoveredIndex(index);
+                                setVisibleIndices(prev => ({ ...prev, [index]: true }));
+                            }}
+                            onMouseLeave={() => {
+                                setHoveredIndex(null);
+                                setVisibleIndices(prev => ({ ...prev, [index]: false }));
+                            }}
+                        >
+                            <Card.Img 
+                                variant="top" 
+                                src={hoveredIndex === index ? product.img2 : product.img1} 
+                                alt={product.name} 
+                                className='rounded-2xl' 
+                            />
                             {visibleIndices[index] && <IconMenu />}
                         </div>
                         <Card.Body>
-                            <Card.Title className='productNameStyle font-medium '>{product.name}</Card.Title>
+                            <Card.Title className='productNameStyle font-medium'>{product.name}</Card.Title>
                             <Card.Text>
                                 <div className="flex">
                                     <div className='flex gap-1'>
-                                        {[...Array(5)].map((star, index) => (
-                                            <FaStar key={index} color="red" />
+                                        {[...Array(5)].map((star, idx) => (
+                                            <FaStar key={idx} color="red" />
                                         ))}
                                     </div>
                                     <span className="ml-5">{product.rev}</span>

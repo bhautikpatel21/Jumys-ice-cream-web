5d4fb9d518d884ad759443a45eb067ec64addc6d
Bhautik Dudhat <Bdudhat293@gmail.com>
2024-10-07 10:00:13 +0530

    GR-ap

--- jumys-ice-cream/src/App.jsx
+++ jumys-ice-cream/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <>
 
-    {/* <Layout/> */}
+    <Layout/>
     {/* <ProductCardList/> */}
-    <ShopList/>
+    {/* <ShopList/> */}
     {/* <ProductCard/> */}
     </>
     

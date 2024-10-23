17c8174c6574bb75beea2661b89d80ac838f79e2
bhautik dudhat <bdudhat293@gmail.com>
2024-10-23 19:57:03 +0530

    cart page

--- jumys-ice-cream/src/pages/CartPage.jsx
+++ jumys-ice-cream/src/pages/CartPage.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
 import HomeFooter from '../Components/Footer2';
 import ScrollToTopButton from '../Components/TopButton';
+import Home from '../pages/Home';
+import { NavLink,Outlet } from 'react-router-dom';
 
 const CartPage = () => {
   const [isPageLoaded, setIsPageLoaded] = useState(false);
@@ -21,11 +23,12 @@ const CartPage = () => {
           Your Cart Is Currently Empty
         </h1>
         <br />
-        
+        <NavLink to="/">
         <button className='flex items-center bg-black text-white px-5 py-3 md:px-7 md:py-4 gap-3 rounded-full hover:bg-red-600 hover:duration-500'>
           <FaArrowLeft className='mt-1' />
-          <span className="text-sm md:text-base">Return To Shop</span>
+          <span className="text-sm md:text-base">BACK TO HOME PAGE</span>
         </button>
+        </NavLink>
       </div>
       <HomeFooter />
       <ScrollToTopButton/>

1930a7e730397c09bf8b358c49aafe3c4dab903b
bhautik dudhat <bdudhat293@gmail.com>
2024-10-30 22:10:23 +0530

    login page

--- jumys-ice-cream/src/pages/Login.jsx
+++ jumys-ice-cream/src/pages/Login.jsx
@@ -1,26 +1,28 @@
-import React, { useState, useEffect } from 'react';
+//////////////////////////////////////////////////////////////////////////////////////////
+
+import React, { useState, useEffect } from "react";
 import backgroundImage from "../assets/asset 50.jpeg";
-import { FaUser } from 'react-icons/fa';
-import Footer2 from '../Components/Footer2';
-import ScrollToTopButton from '../Components/TopButton';
-import axios from 'axios';
+import { FaUser } from "react-icons/fa";
+import Footer2 from "../Components/Footer2";
+import ScrollToTopButton from "../Components/TopButton";
+import axios from "axios";
 
 const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [isPageLoaded, setIsPageLoaded] = useState(false);
-  const [loginEmail, setLoginEmail] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
-  const [registerUsername, setRegisterUsername] = useState('');
-  const [registerEmail, setRegisterEmail] = useState('');
-  const [registerPassword, setRegisterPassword] = useState('');
-  const [error, setError] = useState('');
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  const [showLogin, setShowLogin] = useState(false); // Start with false to show registration form first
+  const [showLogin, setShowLogin] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsPageLoaded(true);
-    }, 100); 
+    }, 100);
 
     return () => clearTimeout(timer);
   }, []);
@@ -32,18 +34,21 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
+    setError("");
 
     try {
-      const response = await axios.post('http://localhost:7410/api/user/login/login-user', {
-        email: loginEmail,
-        password: loginPassword,
-      });
-      console.log('Login Success:', response.data);
-      alert('Login successfully');
+      const response = await axios.post(
+        "http://localhost:7410/api/user/login/login-user",
+        {
+          email: loginEmail,
+          password: loginPassword,
+        }
+      );
+      console.log("Login Success:", response.data);
+      alert("Login successfully");
       // Handle successful login, e.g., redirect or store token
     } catch (err) {
-      alert('Login failed. Please check your credentials.');
+      alert("Login failed. Please check your credentials.");
     } finally {
       setLoading(false);
     }
@@ -52,26 +57,29 @@ const Login = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
+    setError("");
 
     try {
-      const response = await axios.post('http://localhost:7410/api/user/login/register', {
-        username: registerUsername,
-        email: registerEmail,
-        password: registerPassword,
-      });
-      console.log('Registration Success:', response.data);
-      alert('Registered successfully. Please log in.');
+      const response = await axios.post(
+        "http://localhost:7410/api/user/login/register",
+        {
+          username: registerUsername,
+          email: registerEmail,
+          password: registerPassword,
+        }
+      );
+      console.log("Registration Success:", response.data);
+      alert("Registered successfully. Please log in.");
       setShowLogin(true); // Show login form after successful registration
     } catch (err) {
-      alert('Registration failed. Please try again.');
+      alert("Registration failed. Please try again.");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className={`relative ${isPageLoaded ? 'fade-in' : ''}`}>
+    <div className={`relative ${isPageLoaded ? "fade-in" : ""}`}>
       <div className="relative w-full h-60 sm:h-80 md:h-[400px]">
         <img
           src={backgroundImage}
@@ -80,9 +88,16 @@ const Login = () => {
         />
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="text-center text-white px-4 py-6">
-            <h1 className="text-3xl md:text-4xl text-black mb-2" id="manufacture">My Account</h1>
+            <h1
+              className="text-3xl md:text-4xl text-black mb-2"
+              id="manufacture"
+            >
+              My Account
+            </h1>
             <p className="text-sm md:text-base">
-              <a href="#" className="text-black hover:text-red-500">Home</a>{" "}
+              <a href="#" className="text-black hover:text-red-500">
+                Home
+              </a>{" "}
               <span className="text-black">/</span>
               <span className="text-red-500">My Account</span>
             </p>
@@ -90,10 +105,12 @@ const Login = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row gap-8 p-6 md:p-10 mt-10">
+      <div className="flex flex-col md:flex-row gap-8 p-6 md:p-10 mt-10 items-center">
         {showLogin ? (
+          
           // Login Form
-          <div className="p-6 md:p-12 bg-white border border-gray-200 rounded-lg shadow-lg flex-1 slide-up">
+
+          <div className="w-full max-w-md mx-auto md:max-w-lg lg:max-w-xl xl:max-w-2xl p-6 md:p-12 bg-white border border-gray-200 rounded-lg shadow-lg flex-1 slide-up">
             <div className="flex items-center text-lg md:text-2xl gap-x-2 mb-6 underline">
               <FaUser />
               <h2 className="font-bold text-gray-800">Login</h2>
@@ -129,8 +146,7 @@ const Login = () => {
                     type="button"
                     className="absolute inset-y-0 right-0 px-3 text-gray-700 focus:outline-none"
                     onClick={togglePasswordVisibility}
-                  >
-                  </button>
+                  ></button>
                 </div>
               </div>
               <div className="flex items-center justify-between mb-6">
@@ -153,17 +169,21 @@ const Login = () => {
               </div>
               {error && <div className="text-red-500">{error}</div>}
               <button
-                className={`w-full px-4 py-2 font-bold text-white bg-black rounded-full hover:bg-red-500 transition duration-300 focus:outline-none focus:shadow-outline ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                className={`w-full px-4 py-2 font-bold text-white bg-black rounded-full hover:bg-red-500 transition duration-300 focus:outline-none focus:shadow-outline ${
+                  loading ? "opacity-50 cursor-not-allowed" : ""
+                }`}
                 type="submit"
                 disabled={loading}
               >
-                {loading ? 'Loading...' : 'LOGIN'}
+                {loading ? "Loading..." : "LOGIN"}
               </button>
             </form>
           </div>
         ) : (
+
           // Register Form
-          <div className="p-6 md:p-12 bg-white border border-gray-200 rounded-lg shadow-lg flex-1 slide-up">
+
+          <div className="w-full max-w-md mx-auto md:max-w-lg lg:max-w-xl xl:max-w-2xl p-6 md:p-12 bg-white border border-gray-400 rounded-lg shadow-lg flex-1 slide-up">
             <div className="flex items-center text-lg md:text-2xl gap-x-2 mb-6 underline">
               <FaUser />
               <h2 className="font-bold text-gray-800">Register</h2>
@@ -174,7 +194,7 @@ const Login = () => {
                   User name <span className="text-red-500">*</span>
                 </label>
                 <input
-                  className="w-full px-3 py-2 border-2 border-gray-400 md:py-3 leading-tight text-gray-700 rounded-full focus:outline-none focus:shadow-outline"
+                  className="w-full mb-5 px-3 py-2 border-2 border-gray-400 md:py-3 leading-tight text-gray-700 rounded-full focus:outline-none focus:shadow-outline"
                   id="name"
                   type="text"
                   value={registerUsername}
@@ -206,21 +226,44 @@ const Login = () => {
                     onChange={(e) => setRegisterPassword(e.target.value)}
                     required
                   />
+
                   <button
                     type="button"
                     className="absolute inset-y-0 right-0 px-3 text-gray-700 focus:outline-none"
                     onClick={togglePasswordVisibility}
                   >
+                    <svg
+                      className="w-6 h-6"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-.172.527-.376 1.032-.606 1.5M15 12a3 3 0 01-6 0"
+                      />
+                    </svg>
                   </button>
                 </div>
               </div>
               {error && <div className="text-red-500">{error}</div>}
               <button
-                className={`w-full px-4 py-2 mt-6 font-bold text-white bg-gray-400 rounded-full hover:bg-red-500 transition duration-300 focus:outline-none focus:shadow-outline ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                className={`w-full px-4 py-2 mt-6 font-bold text-white bg-gray-400 rounded-full hover:bg-red-500 transition duration-300 focus:outline-none focus:shadow-outline ${
+                  loading ? "opacity-50 cursor-not-allowed" : ""
+                }`}
                 type="submit"
                 disabled={loading}
               >
-                {loading ? 'Loading...' : 'REGISTER'}
+                {loading ? "Loading..." : "REGISTER"}
               </button>
             </form>
           </div>

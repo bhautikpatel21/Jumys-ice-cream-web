d784b2bd693dddb839ff319712f2eb71ee4badf7
bhautik dudhat <bdudhat293@gmail.com>
2024-11-01 19:10:40 +0530

    login

--- jumys-ice-cream/src/pages/Login.jsx
+++ jumys-ice-cream/src/pages/Login.jsx
@@ -1,5 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////////////
-
 import React, { useState, useEffect } from "react";
 import backgroundImage from "../assets/asset 50.jpeg";
 import { FaUser } from "react-icons/fa";

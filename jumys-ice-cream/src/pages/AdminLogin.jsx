3a4d17cb60577a637027923e0fb545d414e3cf17
Bhautik Dudhat <bdudhat293@gmail.com>
2024-11-20 11:52:53 +0530

    users all

--- jumys-ice-cream/src/pages/AdminLogin.jsx
+++ jumys-ice-cream/src/pages/AdminLogin.jsx
@@ -232,6 +232,10 @@ const AdminLogin = () => {
       >
         Change Password
       </button>
+
+      <button
+        className="bg-yellow-600 text-white px-4 py-2 mt-4 rounded"
+      >Show All Users </button>
     </div>
   );
 

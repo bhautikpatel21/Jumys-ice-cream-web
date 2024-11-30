4fcf1cd0e847b914f494406efa67264c261ffb9d
bhautik dudhat <bdudhat293@gmail.com>
2024-11-30 14:56:43 +0530

    add product backend complated

--- jumys-BackEnd/src/Routes/Admin/Index.Routes.js
+++ jumys-BackEnd/src/Routes/Admin/Index.Routes.js
@@ -1,10 +1,10 @@
 const adminRoutes = require('express').Router();
 const loginRoutes = require('./Login.Routes');
-const productRoutes = require('./Product.Routes');
 const cartRoutes = require('./Cart.Routes');
+const Productrouter = require('./Product.Routes')
 
 adminRoutes.use('/login',loginRoutes);
-adminRoutes.use('/product',productRoutes);
+adminRoutes.use('/product',Productrouter);
 adminRoutes.use('/cart',cartRoutes);
 
 module.exports = adminRoutes;
--- jumys-BackEnd/src/Routes/Admin/Product.Routes.js
+++ jumys-BackEnd/src/Routes/Admin/Product.Routes.js
@@ -1,24 +1,47 @@
-const express = require('express');
-const productRoutes = express.Router();
+// const express = require('express');
+// const productRoutes = express.Router();
 
-const {
+// const {
     
-    addNewProduct,
-    getAllProducts,
-    getProduct,
-    updateProduct,
-    deleteProduct
+//     addNewProduct,
+//     getAllProducts,
+//     getProduct,
+//     updateProduct,
+//     deleteProduct
 
-} = require('../../Controller/Admin/Product.controller');
+// } = require('../../Controller/Admin/Product.controller');
 
-productRoutes.post('/add-product', addNewProduct);
+// productRoutes.post('/add-product', addNewProduct);
 
-productRoutes.get('/get-All-product', getAllProducts);
+// productRoutes.get('/get-All-product', getAllProducts);
 
-productRoutes.get('/get-product', getProduct);
+// productRoutes.get('/get-product', getProduct);
 
-productRoutes.put('/update-product', updateProduct);
+// productRoutes.put('/update-product', updateProduct);
 
-productRoutes.delete('/delete-product', deleteProduct);
+// productRoutes.delete('/delete-product', deleteProduct);
 
-module.exports = productRoutes;
+// module.exports = productRoutes;
+
+
+const express = require('express');
+const ProductController = require('../../Controller/Admin/Product.controller');
+
+const Productrouter = express.Router();
+
+// Add product
+Productrouter.post('/add-product', ProductController.addProduct);
+
+// Get a single product by productId
+Productrouter.get('/:productId', ProductController.getProduct);
+
+// Get all products
+Productrouter.get('/', ProductController.getAllProducts);
+
+// Update product
+Productrouter.put('/:productId', ProductController.updateProduct);
+
+// Delete product
+Productrouter.delete('/:productId', ProductController.deleteProduct);
+
+module.exports = Productrouter;

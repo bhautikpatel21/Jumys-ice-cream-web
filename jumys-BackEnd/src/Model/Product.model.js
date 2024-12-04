aebb4832c196f882594ca1dfa563511da18bc99a
bhautik dudhat <bdudhat293@gmail.com>
2024-12-04 20:47:50 +0530

    that time product add successfully

--- jumys-BackEnd/src/Model/Product.model.js
+++ jumys-BackEnd/src/Model/Product.model.js
@@ -1,44 +1,54 @@
+// const mongoose = require('mongoose');
+
+// const productSchema = mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   rev: {
+//     type: String,
+//     required: true
+//   },
+//   img1: {
+//     type: String,
+//     required: true
+//   },
+//   img2: {
+//     type: String,
+//     required: true
+//   },
+//   ProductId: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   size: {
+//     type: [String],  // Array of strings for sizes (like 'S', 'M', 'L','XL')
+//     required: true
+//   },
+//   imageList: {
+//     type: [String],  // Array of image URLs or paths
+//     required: true
+//   }
+// });
+
+// const Product = mongoose.model('Products', productSchema);
+
+// module.exports = Product;
+
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price : {
-    type : Number,
-    required : true
-  },
-  color : {
-    type : String,
-    required : true
-  },
-  mainImage : {
-    type : String,
-    required : true
-  },
-  otherImages : {
-    type : [String],
-  },
-  rev: {
-    type: String,
-    required: true
-  },
-  ProductId: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  sizes: {
-    type: [String],  // Array of strings for sizes (like 'S', 'M', 'L','XL')
-    required: true
-  },
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    sizes: [String],
+    review: { type: String },
+    color: { type: String, required: true },
+    // mainImage: { type: String, required: true },
+    // otherImages: [String],
+    ProductId: { type: String, unique: true, required: true },
+}, { timestamps: true });
 
-},{
-    versionKey : false,
-    timeStamp : true
-});
-
-const Product = mongoose.model('Products', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
